c5043658270f137b99f7152997431f7f59e57dc3
Yanislav Vasilev <ianislav_vasilev@abv.bg>
2017-05-20 22:29:40 +0300

    IDisposablePattern initial commit , need to provide dummy connection string to work.

---
+++ IDisposable/IDisposablePattern/IDisposablePattern/DatabaseState.cs
@@ -0,0 +1,55 @@
+﻿namespace IDisposablePattern
+{
+    using System;
+    using System.Data.SqlClient;
+
+    // Couple of things to notice here.
+    // 
+    // 1. We are creating type that holds instance of a type that implements IDisposable interface 
+    //  this should ring us a bell that we should implement IDisposable for our type as well!
+    // 
+    // 2. Notice how we are using this type but we are not closing the connection anywhere, this can be done for optimization
+    //  purposes. ( Having to close and open MySQL connection each time is relatively expensive so we want to keep the connection open
+    //  and re-use it for later ). Whats the problem with this? Well if we are running our application on multiple instances 
+    //  concurrently we might overflow the connection pool set to our SQL Server and this makes our application not scalable.
+    //
+    // 3. Luckily our class implements IDisposable pattern so drawback from point 2, shouldnt be a problem if it is used CORRECTLY.
+    // (in using statement for instance!!!)
+    //
+    public class DatabaseState : IDisposable
+    {
+        private SqlConnection _connection;
+
+        public string GetDate()
+        {
+            if (this._connection == null)
+            {
+                // Dummy operations
+                _connection = new SqlConnection();
+                _connection.Open();
+            }
+            using (var command = _connection.CreateCommand())
+            {
+                command.CommandText = "SELECT getdate()";
+                return command.ExecuteScalar().ToString();
+            }
+        }
+
+        /// <summary>
+        ///     This method gets called when <see cref="DatabaseState"/> object 
+        ///     is created within using statement.
+        /// </summary>
+        /// <remarks> 
+        ///     This should right a bell however, if we are not using our <see cref="DatabaseState"/> instance
+        ///     like we are supposed to we will never close our connections as said in point 3.
+        /// 
+        ///     This is actually not the best practice of implementing IDisposable. 
+        ///     Refer to <see cref="DatabaseStateImpr"/> for better implementation.
+        /// </remarks>
+        public void Dispose()
+        {
+            _connection.Close();
+            _connection.Dispose();
+        }
+    }
+}
---
+++ IDisposable/IDisposablePattern/IDisposablePattern/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace IDisposablePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Just a note:
+            // When using "using" statements , this gets compiled to IL try/finally block
+            // which executes virtual method Dispose() to the object created in using.
+            using (var connection = new DatabaseState())
+            {
+                connection.GetDate();
+            }
+        }
+    }
+}
