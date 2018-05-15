61db6318f735134fdb7997fb50bb7e45932cf43c
kuskmen <ianislav_vasilev@abv.bg>
2018-05-15 23:45:43 +0300

    Reordered solution structure.

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -9,7 +9,7 @@ namespace Sandbox
             var arr = new[] { 2, 7, 26, 25, -19, -17, 1, 90, 3, 36 };
             var heap = new MaxHeap<int>(arr, (x, y) => x > y ? 1 : y > x ? -1 : 0);
 
-            heap.Add(5897);
+            heap.Remove(1);
 
         }
     }
